90783ad476b463de7ac23c95cba243ac1a64ea3c
Artur Curinga <arturmcuringa@gmail.com>
2018-11-12 15:10:06 -0300

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='facenet',
+	version='0.0.1',
+	description="Face recognition with Google's FaceNet deep neural network.",
+	url='https://github.com/arturcuringa/facenet.git',
+	packages=['facenet'],
+)
+
