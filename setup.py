32904dde785cad615718f2afc73cb216e46f3e89
Artur Curinga <arturmcuringa@gmail.com>
2018-11-13 14:07:45 -0300

    Update setup

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
 	version='0.0.2',
 	description="Face recognition with Google's FaceNet deep neural network.",
 	url='https://github.com/arturcuringa/facenet.git',
-	packages=['facenet', 'src', 'align'],
+	packages=['facenet', 'facenet.src', 'facenet.src.align'],
 )
 
