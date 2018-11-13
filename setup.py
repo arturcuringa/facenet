6b5d68d2c06f4d009579cce170ab501400077ae0
Artur Curinga <arturmcuringa@gmail.com>
2018-11-13 14:05:02 -0300

    Upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='facenet',
-	version='0.0.1',
+	version='0.0.2',
 	description="Face recognition with Google's FaceNet deep neural network.",
 	url='https://github.com/arturcuringa/facenet.git',
 	packages=['facenet'],
