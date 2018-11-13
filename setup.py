9124f6a2afbd1d23bc3de4c9281da5aff044625d
Artur Curinga <arturmcuringa@gmail.com>
2018-11-13 15:39:37 -0300

    Add align data

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
 	description="Face recognition with Google's FaceNet deep neural network.",
 	url='https://github.com/arturcuringa/facenet.git',
 	packages=['facenet', 'facenet.src', 'facenet.src.align'],
+	package_data={'facenet.src.align': ['*.npy']}
 )
 
