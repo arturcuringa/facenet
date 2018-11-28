163b5871fc2fbf234d87c8fe41c3293a101d6ce1
Artur Curinga <arturmcuringa@gmail.com>
2018-11-28 17:28:50 -0300

    Add contributed to facenet package

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
 	name='facenet',
-	version='0.0.2',
+	version='0.0.3',
 	description="Face recognition with Google's FaceNet deep neural network.",
 	url='https://github.com/arturcuringa/facenet.git',
-	packages=['facenet', 'facenet.src', 'facenet.src.align'],
+	packages=['facenet', 'facenet.contributed' , 'facenet.src', 'facenet.src.align'],
 	package_data={'facenet.src.align': ['*.npy']}
 )
 
